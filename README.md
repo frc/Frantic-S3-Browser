b7ae34e56190f174b624985d5e7ead826fd282d5
Oskari Okko Ojala <okko@frantic.com>
2012-11-30 01:29:48 +0200

    initial commit

--- README.md
+++ README.md
@@ -1,4 +1,115 @@
-Amazon-S3-HTML5-file-browser-and-uploader
-=========================================
+HTML5-file-browser-and-uploader-for-Amazon-S3
+=============================================
 
 File browser and uploader for Amazon S3. Fully HTML5 based, no server needed.
+
+Setup
+=========
+*Add the bucket
+Add a new bucket using https://console.aws.amazon.com/s3/home - pay attention
+that you place the bucket in the region you want it to be in.
+
+*Add Bucket Policy for Website Access
+Select the bucket, press "Properties". In the Permissions tab press
+"Add Bucket Policy". Paste this in, replacing "YOURBUCKETNAME" with the name
+of your bucket:
+
+{
+    "Version": "2008-10-17",
+    "Statement": [
+             {
+                "Sid": "PublicReadGetObject",
+                "Effect": "Allow",
+                "Principal": {
+                    "AWS": "*"
+                },
+                "Action": "s3:GetObject",
+                "Resource": "arn:aws:s3:::YOURBUCKETNAME/*"
+             }
+    ]
+}
+
+Save the policy.
+
+*Enable Website Access
+Select the "Website Access" tab in Properties. Check Enabled [X] and specify
+"index.html" as the Index Document. Note the endpoint Amazon gives to you and
+write it down. Press save.
+
+*Add a CORS policy to enable API requests
+Go back to Permissions tab and press "Add a CORS policy". Paste this in:
+
+<?xml version="1.0" encoding="UTF-8"?>
+<CORSConfiguration xmlns="http://s3.amazonaws.com/doc/2006-03-01/">
+    <CORSRule>
+        <AllowedOrigin>*</AllowedOrigin>
+        <AllowedMethod>POST</AllowedMethod>
+        <AllowedMethod>GET</AllowedMethod>
+        <AllowedHeader>*</AllowedHeader>
+    </CORSRule>
+</CORSConfiguration>
+
+*Add the user to access the bucket. Go to IAM, add a new user.
+Save the Security Credentials, you will need them later in the setup.
+Select the user's Permissions tab and add a Policy as follows:
+
+TODO: Not all s3: actions are needed, just bucketlist, putobject?
+
+{
+  "Statement": [
+    {
+      "Sid": "Stmt1353931095381",
+      "Action": [
+        "s3:*"
+      ],
+      "Effect": "Allow",
+      "Resource": [
+   "arn:aws:s3:::YOURBUCKETNAME","arn:aws:s3:::YOURBUCKETNAME/*"
+      ]
+    }
+  ]
+}
+
+
+*Setting up Siirrin
+Rename the "rename-me" folder to something hard to guess. The folder name
+works as a password. If someone guesses the folder names and can guess
+the file names in your folder they will be able to access the files
+anonymously through the Website Access.
+
+*Configure the index.html file in the renamed "rename-me" folder.
+Put in the bucket name and the Access Id of the user.
+Put in the renamed folder name.
+TODO better documentation here.
+
+*Upload the renamed "rename-me" folder to your bucket.
+
+*Access the bucket as
+....?aws_secret_access_key=...
+
+
+Thank you
+=========
+
+Raymond Yee for publishing snippet
+http://blog.mashupguide.net/2008/11/25/amazon-s3-signature-calculation-in-javascript/
+
+
+Copyright
+=========
+
+sha1.js
+ Version 2.2 Copyright Paul Johnston 2000 - 2009.
+ Other contributors: Greg Holt, Andrew Kepert, Ydnar, Lostinet
+ Distributed under the BSD License
+ See http://pajhome.org.uk/crypt/md5 for details.
+
+ - from http://pajhome.org.uk/crypt/md5/sha1.html
+
+sha1-alternative.js
+ Version 2.1a Copyright Paul Johnston 2000 - 2002.
+ Other contributors: Greg Holt, Andrew Kepert, Ydnar, Lostinet
+ Distributed under the BSD License
+ See http://pajhome.org.uk/crypt/md5 for details.
+
+ - from http://pajhome.org.uk/crypt/md5/sha1.js
--- rename-me/siirrin/siirrin-js/siirrin.js
+++ rename-me/siirrin/siirrin-js/siirrin.js
@@ -6,6 +6,7 @@ var Siirrin = function () {
     var secret_dir; // comes in init
     var uploader_container_id; // comes in init
     var aws_access_key_id; // comes in init
+    var aws_secret_access_key; // comes in init
     var aws_signature; // comes inside init
 
     var protocolurl = window.location.protocol + '//';
@@ -18,9 +19,22 @@ var Siirrin = function () {
             aws_signature = as;
     };
 
+    var set_aws_secret_access_key = function (asak) {
+        aws_secret_access_key = asak;
+    };
+
     var make_aws_policy_document = function () {
         aws_policy_document = '{"expiration": "2020-12-01T12:00:00.000Z", "conditions": [{"acl": "' + aws_canned_acl + '"}, {"bucket": "' + bucket_name + '"}, ["starts-with", "$key", "' + secret_dir + '/files/"]]}';
         aws_policy_document_b64 = rstr2b64(aws_policy_document);
+    };
+
+    var sign_api = function (expires) {
+        var http_verb = 'GET';
+        var canonicalized_resource = '/' + bucket_name + '/';
+        var string_to_sign = http_verb + "\n" + '' + "\n" + '' + "\n" + expires + "\n" + '' + canonicalized_resource;
+        // b64pad = "="; // needed for "strict RFC compliance"
+        var sig = b64_hmac_sha1(aws_secret_access_key, string_to_sign);
+        return sig + '=';
     };
 
     var location_querystring = function (location_search) {
@@ -38,13 +52,11 @@ var Siirrin = function () {
     };
 
     var sign = function(aws_secret_access_key, string_to_sign) {
-        //var expires = new Date().valueOf();
-        //expires += 21600; // signed request valid for 6 hours
         var sig = b64_hmac_sha1(aws_secret_access_key, string_to_sign);
         return sig + '=';
         // Authorization: AWS AWSAccessKeyId:Signature
         // http://docs.amazonwebservices.com/AmazonS3/2006-03-01/dev/RESTAuthentication.html
-    }
+    };
 
     var generate_bucket_listing = function (bucketlist_div_selector,files) {
         var out = '<ul class="root">';
@@ -67,15 +79,18 @@ var Siirrin = function () {
         }
         out += "</ul>";
         $(bucketlist_div_selector).html(out);
-    }
+    };
 
     var init_bucketlist = function (bucketlist_div_selector) {
-        var signature = 'mamemimo';
+        var expires = new Date().valueOf();
+        expires = parseInt(expires/1000); // milliseconds to seconds
+        expires += 21600; // signed request valid for 6 hours
+        var signature = sign_api(expires);
         $(function() {
                 $.ajax({
                         url: protocolurl + bucket_name + s3url,
+                        data: {'AWSAccessKeyId': aws_access_key_id, 'Signature': signature, 'Expires': expires},
                         dataFormat: 'xml',
-                        headers: {'Authorization': 'AWS ' + aws_access_key_id + ":" + signature},
                         success: function(data) {
                             var contents = $(data).find('Contents');
                             var files = [];
@@ -87,7 +102,7 @@ var Siirrin = function () {
                         }
                     });
             });
-    }
+    };
 
     var init_fileupload_field = function (fileupload_field_selector) {
         $(fileupload_field_selector).fileupload({
@@ -102,7 +117,7 @@ var Siirrin = function () {
                     signature: aws_signature,
                 }
             });
-    }
+    };
 
     return {
         set_aws_access_key_id: function (aaki) {
@@ -119,17 +134,13 @@ var Siirrin = function () {
 
             var qs = location_querystring(args.qs);
             if (qs.aws_secret_access_key) {
-                // generate new signature using the aws_secret_access_key, redirect there.
-                var aws_signature = sign(qs.aws_secret_access_key, aws_policy_document_b64);
-                window.location.replace(location.protocol + '//' + location.host + location.pathname + '?as=' + encodeURIComponent(aws_signature));
-            } else {
-                set_aws_signature(qs.as);
-                init_bucketlist(args.bucketlist_div);
-                init_fileupload_field(args.fileupload_field);
+                set_aws_secret_access_key(qs.aws_secret_access_key);
+                set_aws_signature(sign(qs.aws_secret_access_key, aws_policy_document_b64));
             }
+            init_bucketlist(args.bucketlist_div);
+            init_fileupload_field(args.fileupload_field);
         }
-    }
-
+    };
 };
 
 //  Load page with your secret access key as the query string. you will get a signature. Do not share your secret access key or write it in the files.
